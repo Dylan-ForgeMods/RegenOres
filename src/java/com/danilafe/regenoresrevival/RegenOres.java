26e3991fffd96bdbc14cde7708f30c8fedcf9c85
Danila Fedorin <danila.fedorin@gmail.com>
2016-02-15 15:02:47 -0800

    Configuration

--- src/java/com/danilafe/regenoresrevival/RegenOres.java
+++ src/java/com/danilafe/regenoresrevival/RegenOres.java
@@ -10,6 +10,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -30,6 +31,8 @@ public class RegenOres {
 	@Instance(MODID)
 	public static RegenOres instance;
 	
+	public Configuration config;
+	
 	public Block dormantOre;
 	
 	@EventHandler
@@ -37,7 +40,15 @@ public class RegenOres {
 		dormantOre = new DormantOre(Material.rock);
 		
 		GameRegistry.registerTileEntity(DormantOreTileEntity.class, "dormantOreTileEntity");
-		GameRegistry.registerBlock(dormantOre, "dormantOre");	
+		GameRegistry.registerBlock(dormantOre, "dormantOre");
+
+		config = new Configuration(event.getSuggestedConfigurationFile());
+		int    rate   = config.get(Configuration.CATEGORY_GENERAL, "TickRate", 10, "How many random ticks it takes for the ore to regenerate.").getInt();
+		double chance = config.get(Configuration.CATEGORY_GENERAL, "SuccessChance", 1F, "The success chance, between 0 and 1. 1 means 100% success chance, 0 means 0%").getDouble();	
+		if(config.hasChanged()) config.save();
+		
+		((DormantOre) dormantOre).setSuccessChance(chance);
+		((DormantOre) dormantOre).setTransformRate(rate);
 		
 	}
 	
--- src/java/com/danilafe/regenoresrevival/block/DormantOre.java
+++ src/java/com/danilafe/regenoresrevival/block/DormantOre.java
@@ -14,6 +14,9 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class DormantOre extends BlockContainer {
 
+	protected double successChance = 1;
+	protected int transformRate = 10;
+	
 	public DormantOre(Material materialIn) {
 		super(materialIn);
 		
@@ -38,9 +41,28 @@ public class DormantOre extends BlockContainer {
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
 		super.updateTick(worldIn, pos, state, rand);
 		
+		if(rand.nextInt(transformRate) != 0) return; 
+		
 		DormantOreTileEntity tileEntity = (DormantOreTileEntity) worldIn.getTileEntity(pos);
 		Block toSet = GameRegistry.findBlock(tileEntity.getBlockId().substring(0, tileEntity.getBlockId().indexOf(':')), tileEntity.getBlockId().substring(tileEntity.getBlockId().indexOf(':') + 1));
-		worldIn.setBlockState(pos, toSet.getDefaultState());
+		if(rand.nextDouble() <= successChance) worldIn.setBlockState(pos, toSet.getDefaultState());
+		else worldIn.setBlockToAir(pos);
+	}
+
+	public double getSuccessChance() {
+		return successChance;
+	}
+
+	public void setSuccessChance(double successChance) {
+		this.successChance = successChance;
+	}
+
+	public int getTransformRate() {
+		return transformRate;
+	}
+
+	public void setTransformRate(int successRate) {
+		this.transformRate = successRate;
 	}
 
 }
