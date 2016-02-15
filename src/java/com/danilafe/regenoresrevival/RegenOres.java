8a3bccadb64dbb960fbe4d01b4b17d469e7aff07
Danila Fedorin <danila.fedorin@gmail.com>
2016-02-15 01:07:10 -0800

    Regiter in init, change a few things around

--- src/java/com/danilafe/regenoresrevival/RegenOres.java
+++ src/java/com/danilafe/regenoresrevival/RegenOres.java
@@ -1,20 +1,21 @@
 package com.danilafe.regenoresrevival;
 
 import com.danilafe.regenoresrevival.block.DormantOre;
+import com.danilafe.regenoresrevival.block.DormantOreTileEntity;
 import com.danilafe.regenoresrevival.event.EventListener;
 import com.danilafe.regenoresrevival.proxies.CommonProxy;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = RegenOres.MODID, version = RegenOres.VERSION)
@@ -26,19 +27,23 @@ public class RegenOres {
 	@SidedProxy(clientSide="com.danilafe.regenoresrevival.proxies.ClientProxy", serverSide="com.danilafe.regenoresrevival.proxies.ServerProxy")
 	public static CommonProxy proxy;
 	
-	public static Block dormantOre = new DormantOre(Material.rock);
+	@Instance(MODID)
+	public static RegenOres instance;
+	
+	public Block dormantOre;
 	
 	@EventHandler
-	public static void preInit(FMLPreInitializationEvent event){
+	public void preInit(FMLPreInitializationEvent event){
+		dormantOre = new DormantOre(Material.rock);
 		
+		GameRegistry.registerTileEntity(DormantOreTileEntity.class, "dormantOreTileEntity");
 		GameRegistry.registerBlock(dormantOre, "dormantOre");	
-		MinecraftForge.EVENT_BUS.register(new EventListener());
 		
 	}
 	
 	@EventHandler
-	public static void init(FMLInitializationEvent event) {
-		
+	public void init(FMLInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(new EventListener());
 	}
 	
 }
